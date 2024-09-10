438659c64c266bd582e9894387a21a7db9d07d55
Phil <ephrones@msudenver.edu>
2024-09-10 15:02:47 -0600

    sc2

--- sc2/main.py
+++ sc2/main.py
@@ -10,3 +10,11 @@ if choice == 'f2m':
     print(f'{value}ft = {feet_to_meters(value)}mt')
 else: 
     print(f'{value}mt = {meters_to_feet(value)}ft')
+    
+    import math 
+
+def feet_to_meters(value):
+    return math.ceil(value * 0.3048 * 100) / 100
+
+def meters_to_feet(value):
+    return math.ceil(value / 0.3048 * 100) / 100
