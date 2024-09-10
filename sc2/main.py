c8244e9e586a1747361dacbb30cf675131ec7a68
Phil <ephrones@msudenver.edu>
2024-09-10 15:00:02 -0600

    sc2

---
+++ sc2/main.py
@@ -0,0 +1,12 @@
+def feet_to_meters(value):
+    return value * 0.3048
+
+def meters_to_feet(value):
+    return value / 0.3048
+
+choice = input('f2m or m2f? ')
+value = float(input('? '))
+if choice == 'f2m':
+    print(f'{value}ft = {feet_to_meters(value)}mt')
+else: 
+    print(f'{value}mt = {meters_to_feet(value)}ft')
